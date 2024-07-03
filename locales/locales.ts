2954c488cf849cb4480d5392b07114a594990008
onury5506 <onuryildizsai@gmail.com>
2024-07-03 19:29:31 +0300

    chore: Add localization support with expo-localization

---
+++ locales/locales.ts
@@ -0,0 +1,19 @@
+import { getLocales } from 'expo-localization';
+import { I18n } from 'i18n-js';
+import english from './files/en.json';
+import turkish from './files/tr.json';
+
+export const i18n = new I18n({
+    en: english,
+    tr: turkish,
+});
+
+const deviceLanguage = getLocales()[0].languageCode ?? 'en';
+
+console.log(getLocales())
+
+if (!i18n.translations[deviceLanguage]) {
+    i18n.locale = 'en';
+}else{
+    i18n.locale = deviceLanguage;
+}
