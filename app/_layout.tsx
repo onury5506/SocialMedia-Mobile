099cd18efc4ad187ba2eb64e6c62a6bbab5ffef8
onury5506 <onuryildizsai@gmail.com>
2024-07-03 21:11:04 +0300

    Integrate paper theme provider and dark/light theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,23 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { MD2DarkTheme, MD2LightTheme, PaperProvider } from 'react-native-paper';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+const darkTheme = {
+  ...MD2DarkTheme,
+  colors: {
+    ...MD2DarkTheme.colors,
+  },
+};
+
+const lightTheme = {
+  ...MD2LightTheme,
+  colors: {
+    ...MD2LightTheme.colors,
+  },
+};
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -26,13 +41,16 @@ export default function RootLayout() {
     return null;
   }
 
+  console.log(darkTheme.colors.surface, lightTheme.colors.surface)
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <PaperProvider theme={false ? darkTheme : lightTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false}} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
+    </PaperProvider>
   );
 }
