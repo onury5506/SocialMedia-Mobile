0f90c6a8f2fc9b22ca19ed751bf5ec787468dd66
onury5506 <onuryildizsai@gmail.com>
2024-07-05 21:40:52 +0300

    chore: Update auth.api.ts to dispatch user authentication data

--- api/auth.api.ts
+++ api/auth.api.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { LoginDto, LoginResponseDto, RefreshDTO } from "./models";
 import { baseUrl as generalBaseUrl } from "./api.config";
+import store from "@/store/store";
+import { setAuth } from "@/slices/userSlice";
 
 const baseUrl = `${generalBaseUrl}/auth`
 
 export function login(loginDto: LoginDto): Promise<LoginResponseDto> {
     return axios.post(`${baseUrl}/login`, loginDto).then((res)=>{
+        store.dispatch(setAuth(res.data))
         return res.data
     }).catch(err=>{
         throw err?.response?.data
@@ -15,6 +18,7 @@ export function login(loginDto: LoginDto): Promise<LoginResponseDto> {
 
 export async function refresh(refreshDto: RefreshDTO): Promise<LoginResponseDto> {
     return axios.post(`${baseUrl}/refresh`, refreshDto).then((res)=>{
+        store.dispatch(setAuth(res.data))
         return res.data
     }).catch(err=>{
         throw err?.response?.data
