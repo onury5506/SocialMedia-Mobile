783ee155c064196ce6a1cbabb73b68924b6083dc
onury5506 <onuryildizsai@gmail.com>
2024-07-05 21:40:45 +0300

    chore: Add axios interceptor for authorization token

--- api/api.config.ts
+++ api/api.config.ts
@@ -1,1 +1,23 @@
+import { selectAuth } from "@/slices/userSlice";
+import store from "@/store/store";
+import axios from "axios"
+
 export const baseUrl = "http://192.168.1.108:3000"
+
+export const api = axios.create({
+    baseURL: baseUrl
+})
+
+const getToken = () => {
+    const state = store.getState();
+    const auth = selectAuth(state);
+    return auth || null;
+};
+
+api.interceptors.request.use((config)=>{
+    const token = getToken()
+    if(token){
+        config.headers.authorization = token.accessToken
+    }
+    return config
+})
