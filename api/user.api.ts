9b672fb7bd065d0b59524842117a96fe486c813a
onury5506 <onuryildizsai@gmail.com>
2024-07-06 12:23:15 +0300

    Add profilePicture endpoints to user.api.ts

--- api/user.api.ts
+++ api/user.api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { RegisterResponseDTO, RegisterUserDTO, UpdateUserDTO, UserProfileDTO } from "./models";
+import { RegisterResponseDTO, RegisterUserDTO, UpdateUserDTO, UpdateUserProfilePictureDTO, UserProfileDTO } from "./models";
 import { baseUrl as generalBaseUrl } from "./api.config";
 import { api } from "./api.config";
 import store from "@/store/store";
@@ -8,29 +8,66 @@ import { setProfile } from "@/slices/userSlice";
 const baseUrl = `${generalBaseUrl}/user`
 
 export function register(registerDto: RegisterUserDTO): Promise<RegisterResponseDTO> {
-    return axios.post(`${baseUrl}/register`, registerDto).then((res)=>{
-        
+    return axios.post(`${baseUrl}/register`, registerDto).then((res) => {
+
         return res.data
-    }).catch(err=>{
+    }).catch(err => {
         throw err?.response?.data
     })
 }
 
-export function me(): Promise<UserProfileDTO>{
-    return api.get(`${baseUrl}/me`).then((res)=>{
+export function me(): Promise<UserProfileDTO> {
+    return api.get(`${baseUrl}/me`).then((res) => {
         store.dispatch(setProfile(res.data))
         return res.data
-    }).catch(err=>{
+    }).catch(err => {
         throw err?.response?.data
     })
 }
 
-export function updateUser(updateUserDto: UpdateUserDTO): Promise<void>{
-    return api.put(`${baseUrl}/me`, updateUserDto).then(()=>{
+export function updateUser(updateUserDto: UpdateUserDTO): Promise<void> {
+    return api.put(`${baseUrl}/me`, updateUserDto).then(() => {
         return me()
-    }).then(()=>{
+    }).then(() => {
         return;
-    }).catch(err=>{
+    }).catch(err => {
+        throw err?.response?.data
+    })
+}
+
+export function updateProfilePicture(uri: string, filename:string, mimeType: string, size: number): Promise<void> {
+
+    const form = new FormData()
+
+    // @ts-ignore
+    form.append('file', {
+        uri,
+        name: filename,
+        type: mimeType
+    })
+    form.append('left', '0')
+    form.append('top', '0')
+    form.append('size', size.toString())
+    
+    return api.put(`${baseUrl}/me/profilePicture`, form, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(() => {
+        return me()
+    }).then(() => {
+        return;
+    }).catch(err => {
+        throw err?.response?.data
+    })
+}
+
+export function deleteProfilePicture(){
+    return api.delete(`${baseUrl}/me/profilePicture`).then(() => {
+        return me()
+    }).then(() => {
+        return;
+    }).catch(err => {
         throw err?.response?.data
     })
 }
