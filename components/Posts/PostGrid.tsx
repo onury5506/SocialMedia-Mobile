7bc5b69248ca3486c68f75f24e0113316b7d0006
onury5506 <onuryildizsai@gmail.com>
2024-07-07 12:11:05 +0300

    refactor: Update PostGrid component to use expo-image for image rendering

--- components/Posts/PostGrid.tsx
+++ components/Posts/PostGrid.tsx
@@ -1,71 +1,13 @@
-import { PostDataDto } from "@/api/models";
-import React, { useEffect, useMemo, useState } from "react";
-import { View, StyleSheet, Image, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
-import { useTheme } from "react-native-paper";
-import * as VideoThumbnails from 'expo-video-thumbnails';
-
-function PostImageGrid({ url }: { url: string }) {
-    const theme = useTheme()
-    return (
-        <Image
-            source={{ uri: url }}
-            style={{...styles.content, backgroundColor: theme.colors.surface}}
-            resizeMode="cover"
-        />
-    )
-}
-
-function PostVideoGrid({ url }: { url: string }) {
-    const theme = useTheme()
-    const [image, setImage] = useState("");
-
-    const generateThumbnail = async () => {
-        try {
-            const { uri } = await VideoThumbnails.getThumbnailAsync(
-                url,
-                {
-                    time: 1000,
-                }
-            );
-            setImage(uri);
-        } catch (e) {
-            console.warn(e);
-        }
-    };
-
-    useEffect(() => {
-        generateThumbnail();
-    }, []);
-
-    if (image) {
-        return (
-            <Image
-                source={{ uri: image }}
-                style={{...styles.content, backgroundColor: theme.colors.surface}}
-                resizeMode="cover"
-            />
-        )
-    }
-
-    return (
-        <View style={{...styles.content, backgroundColor: theme.colors.surface}}></View>
-    )
-}
+import { PostDataDto, PostDataDtoPostTypeEnum } from "@/api/models";
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Image } from 'expo-image';
 
 export interface PostGridProps extends PostDataDto {
     onPress?: (id: string) => void;
 }
 
-export default function PostGrid({ id, url, postType, onPress }: PostGridProps) {
-    const component = useMemo(() => {
-        if (postType === 'image') {
-            return <PostImageGrid url={url} />
-        } else if (postType === 'video') {
-            return <PostVideoGrid url={url} />
-        }
-        return null
-    }, [postType])
-
+export default function PostGrid({ id, url, postType, blurHash, thumbnail, onPress }: PostGridProps) {
     function handlePress() {
         if (onPress) {
             onPress(id)
@@ -74,7 +16,13 @@ export default function PostGrid({ id, url, postType, onPress }: PostGridProps) 
 
     return (
         <TouchableOpacity style={styles.container} onPress={handlePress}>
-            {component}
+            <Image
+                style={styles.image}
+                source= {postType === PostDataDtoPostTypeEnum.Image ? url : thumbnail}
+                placeholder={{ blurhash: blurHash }}
+                contentFit="cover"
+                transition={1000}
+            />
         </TouchableOpacity>
     )
 }
@@ -84,7 +32,7 @@ const styles = StyleSheet.create({
         width: "33%",
         aspectRatio: 1,
     },
-    content: {
+    image: {
         flex: 1,
         width: "100%",
     }
