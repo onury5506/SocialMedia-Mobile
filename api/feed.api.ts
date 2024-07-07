a679941deb81a64bdd509256c976684dff36d8db
onury5506 <onuryildizsai@gmail.com>
2024-07-07 18:35:48 +0300

    Integrate feed api

---
+++ api/feed.api.ts
@@ -0,0 +1,33 @@
+import { baseUrl as generalBaseUrl } from "./api.config";
+import { api } from "./api.config";
+import { PostDataWithWriterDto } from "./models";
+import { PaginatedDto } from "./paginated.dto";
+
+const baseUrl = `${generalBaseUrl}/feed`
+
+export function getMeFeed(page: number): Promise<PaginatedDto<PostDataWithWriterDto>> {
+    console.log('getMeFeed', `${baseUrl}/me/${page}`)
+    return api.get<PaginatedDto<PostDataWithWriterDto>>(`${baseUrl}/me/${page}`).then(res => {
+        return res.data
+    }).catch(err => {
+        console.log(err)
+        throw err?.response?.data
+    })
+}
+
+export function feedRefresh(): Promise<void> {
+    console.log('feedRefresh', `${baseUrl}/refresh`)
+    return api.post<void>(`${baseUrl}/refresh`).then(res => {
+        return res.data
+    }).catch(err => {
+        throw err?.response?.data
+    })
+}
+
+export function getGlobalFeed(page: number): Promise<PaginatedDto<PostDataWithWriterDto>> {
+    return api.get<PaginatedDto<PostDataWithWriterDto>>(`${baseUrl}/global/${page}`).then(res => {
+        return res.data
+    }).catch(err => {
+        throw err?.response?.data
+    })
+}
