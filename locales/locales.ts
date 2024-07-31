f5834e46700b90d02346dcfec90ab86598f8aa7c
onury5506 <onuryildizsai@gmail.com>
2024-07-31 22:10:54 +0300

    chore: Update locales and moment.js configuration

--- locales/locales.ts
+++ locales/locales.ts
@@ -1,5 +1,7 @@
 import { getLocales } from 'expo-localization';
 import { I18n } from 'i18n-js';
+import 'moment/min/locales';
+import moment from 'moment';
 import english from './files/en.json';
 import turkish from './files/tr.json';
 
@@ -8,15 +10,15 @@ export const i18n = new I18n({
     tr: turkish,
 });
 
-export let {languageCode, languageTag} = getLocales()[0]
+export let { languageCode, languageTag } = getLocales()[0]
 
 const languageTagSplit = languageTag.split('-')
 
-if(languageTagSplit.length > 2){
-    languageTag = languageTagSplit[0]+'-'+languageTagSplit[2].toUpperCase()
-}else if(languageTagSplit.length == 2){
-    languageTag = languageTagSplit[0]+'-'+languageTagSplit[1].toUpperCase()
-}else{
+if (languageTagSplit.length > 2) {
+    languageTag = languageTagSplit[0] + '-' + languageTagSplit[2].toUpperCase()
+} else if (languageTagSplit.length == 2) {
+    languageTag = languageTagSplit[0] + '-' + languageTagSplit[1].toUpperCase()
+} else {
     languageTag = languageTagSplit[0]
 }
 
@@ -24,6 +26,9 @@ const deviceLanguage = getLocales()?.[0]?.languageCode ?? 'en';
 
 if (!i18n.translations[deviceLanguage]) {
     i18n.locale = 'en';
-}else{
+} else {
     i18n.locale = deviceLanguage;
 }
+moment.locale(languageTag.toLowerCase())
+
+export const locale = i18n.locale
