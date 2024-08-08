66d878319881fde5bb20b78cb35fa3fb6008a2fe
onury5506 <onuryildizsai@gmail.com>
2024-08-08 12:01:04 +0300

    refactor: Update MiniProfile component to visit user

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -5,13 +5,16 @@ import { StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 import { follow, unfollow } from "@/api/user.api";
 import { i18n } from "@/locales/locales";
+import VisitUser from "./VisitUser";
 
-export default function MiniProfile({ id, username, name, following, profilePicture }: MiniUserProfile) {
+export interface MiniProfileProps extends MiniUserProfile {
+    replaceUrl?: boolean;
+}
+
+export default function MiniProfile({ id, username, name, following, profilePicture, replaceUrl }: MiniProfileProps) {
     const [isFollowing, setIsFollowing] = useState<boolean>(following)
     const theme = useTheme();
-    const _name = name.length <= 15 ? name : name.substring(0, 12) + "...";
-    const _username = username.length <= 15 ? username : username.substring(0, 12) + "...";
-
+    const _name = name.length <= 20 ? name : name.substring(0, 17) + "...";
     useEffect(() => {
         setIsFollowing(following)
     }, [following])
@@ -32,22 +35,24 @@ export default function MiniProfile({ id, username, name, following, profilePict
 
     return (
         <View style={styles.container}>
-            <View style={styles.nameImageContainer}>
-                <Image
-                    style={styles.profilePicture}
-                    source={profilePicture ? { uri: profilePicture } : require("@/assets/images/noProfilePicture.png")}
-                />
-                <View style={styles.nameContainer}>
-                    <Text style={{
-                        ...styles.nameText,
-                        color: theme.colors.onSurface
-                    }}>{_name}</Text>
-                    <Text style={{
-                        ...styles.userNameText,
-                        color: theme.colors.primary
-                    }}>@{_username}</Text>
+            <VisitUser id={id} username={username} name={name} replaceUrl={replaceUrl}>
+                <View style={styles.nameImageContainer}>
+                    <Image
+                        style={styles.profilePicture}
+                        source={profilePicture ? { uri: profilePicture } : require("@/assets/images/noProfilePicture.png")}
+                    />
+                    <View style={styles.nameContainer}>
+                        <Text style={{
+                            ...styles.nameText,
+                            color: theme.colors.onSurface
+                        }}>{_name}</Text>
+                        <Text style={{
+                            ...styles.userNameText,
+                            color: theme.colors.primary
+                        }}>@{username}</Text>
+                    </View>
                 </View>
-            </View>
+            </VisitUser>
             {isFollowing && <Button style={styles.button} mode="outlined" onPress={handleUnfollow}><Text style={styles.buttonText}>{i18n.t("unfollow")}</Text></Button>}
             {!isFollowing && <Button style={styles.button} mode="outlined" onPress={handleFollow}><Text style={styles.buttonText}>{i18n.t("follow")}</Text></Button>}
         </View>
